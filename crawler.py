66beadac773b0d58b66e82f5c98350e9a4143daf
Roman Dubtsov <dubtsov@gmail.com>
2022-08-24 21:49:35 -0700

    crawler.py: update to the latest html

--- crawler.py
+++ crawler.py
@@ -10,92 +10,49 @@ from urllib.parse import urlparse, urljoin, urlunparse
 from time import sleep
 from bs4 import BeautifulSoup
 
-def getUrl(url, verbose=False):
+def getUrl(url, verbose=True):
     if (verbose):
         from sys import stderr
         stderr.write(f'... fetching {url}\n')
     return requests.get(url)
 
+
+def sanitizeCoverUrl(coverUrl):
+    return coverUrl.split('?')[0]
+
+
 def getPrograms(baseUrl):
+    response = getUrl(baseUrl)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    def worker(baseUrl, pageNum):
+    programs = []
+    for programLink in soup.find_all('a', class_='podcast__item'):
+        programUrl = programLink['href']
+        programTitle = programLink.findChild('h3').text
+        programCover = sanitizeCoverUrl(programLink.findChild('img')['src'])
+        programs.append((programUrl, programTitle, programCover))
 
-        def getBackgroundUrl(div):
-            u = urlparse(div['style'].split('\'')[1])
-            u = u._replace(query='')
-            return urlunparse(u)
-
-        response = getUrl(baseUrl + f'/page/{pageNum}')
-        soup = BeautifulSoup(response.text, 'html.parser')
-        programUrls = [urljoin(baseUrl, link['href'])
-                for link in soup.find_all('a', class_='tales__list-link')]
-        programTitles = [''.join(div.find_all(text=True)).strip()
-                for div in soup.find_all('div', class_='tales__list-anonce-title')]
-        programCoverUrls = [getBackgroundUrl(div)
-                for div in soup.find_all('div', class_='tales__list-img_wrap')]
-
-        return list(zip(programUrls, programTitles, programCoverUrls))
-
-    data_ = []
-    pageNum = 1
-    while True:
-        d = worker(baseUrl, pageNum)
-        if len(d) == 0:
-            break
-        data_.extend(d)
-        pageNum += 1
-        sleep(1)  # Don't be rude
-
-    # Recurse
-    data = []
-    for d in data_:
-        programUrl = d[0]
-        if programUrl.find('program_child') != -1:
-            data.append(d)
-        else:
-            subData = getPrograms(programUrl)
-            data.extend(subData)
-
-    return data
+    return programs
 
 
 def getEpisodes(programUrl):
-    podcastsBaseUrl = 'https://www.deti.fm/more_podcast/uid/'
-    programId = programUrl.split('/')[-1]
-    podcastBaseUrl = urljoin(podcastsBaseUrl, programId)
-
-    def worker(podcastBaseUrl, pageNum):
-        response = getUrl(podcastBaseUrl + f'?page={pageNum}')
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        items = []
-        for name_tag in soup.find_all('div', class_='podcasts__item-name'):
-            title = ''.join(name_tag.find_all(text=True)).strip()
-            url = urljoin(programUrl, name_tag['onclick'].split('\'')[1])
-            if title:
-                items.append((url, title))
-        return items
-
-    pageNum = 1
-    data = []
-    while True:
-        d = worker(podcastBaseUrl, pageNum)
-        pageNum += 1
-        if len(d) == 0:
-            break
-        data.extend(d)
-        sleep(1)  # Don't be rude
-
-    return data
-
-
-def getEpisodeData(episodeUrl):
-    response = getUrl(episodeUrl)
+    response = getUrl(programUrl)
     soup = BeautifulSoup(response.text, 'html.parser')
-    coverUrl = urlparse(soup.find('img', class_='podcasts__screen-pic')['src'])._replace(query='')
-    episodeId = episodeUrl.split('/')[-1]
-    audioUrl = soup.find('audio', id=f'player_{episodeId}')['src']
-    return (audioUrl, urlunparse(coverUrl))
+
+    episodes = []
+    episodeButtons = soup.find_all('button', class_='podcast__play ym')
+    if not episodeButtons:
+        episodeButtons = soup.find_all('button', class_='podcast__play')
+    for episodeButton in episodeButtons:
+        if not episodeButton.has_attr('data-track'):
+            # do not rely on try/except to make errors more visible
+            continue
+        episodeAudioUrl = episodeButton['data-track']
+        episodeTitle = episodeButton['data-track-title']
+        episodeCoverUrl = sanitizeCoverUrl(episodeButton['data-track-cover'])
+        episodes.append((episodeAudioUrl, episodeTitle, episodeCoverUrl))
+
+    return episodes
 
 
 if __name__ == '__main__':
@@ -108,8 +65,8 @@ if __name__ == '__main__':
                 'cover' : programCoverUrl,
                 }
         episodes = []
-        for episodeNum, (episodeUrl, episodeTitle) in enumerate(reversed(getEpisodes(programUrl))):
-            episodeAudioUrl, episodeCoverUrl = getEpisodeData(episodeUrl)
+        # XXX: program order is not stable (?)
+        for episodeNum, (episodeAudioUrl, episodeTitle, episodeCoverUrl) in enumerate(reversed(getEpisodes(programUrl))):
             episodes.append({
                 'num' : episodeNum + 1,
                 'title' : episodeTitle,
